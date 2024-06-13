13f47f21dac7090e81e3637ecde7a23eb74d107f
Hiba ERRAOUI <125480922+er-hiba@users.noreply.github.com>
2024-06-13 10:50:53 +0100

    :)

---
+++ conxDB.php
@@ -0,0 +1,8 @@
+<?php
+$host = 'localhost';
+$dbname = 'company_trips';
+$username = 'your_username';
+$password = 'your_password';
+
+$conn = new PDO("mysql:host=$host; dbname=$dbname" , $username, $password);
+?>
